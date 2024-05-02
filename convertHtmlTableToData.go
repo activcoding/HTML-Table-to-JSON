532700cf5634861f37005ad899dbee15d9a04c13
Tom Ludwig <tommludwig@icloud.com>
2024-05-02 20:33:58 +0200

    Refactor convertHtmlTableToData.go to improve readability and add new conversion functions

--- convertHtmlTableToData.go
+++ convertHtmlTableToData.go
@@ -1,16 +1,24 @@
 package converthtmltabletodata
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
+	"os"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
+// Row represents a single row in a table. It is a map from header string to data string.
 type Row map[string]string
 
+// Table represents a table as a slice of Rows.
 type Table []Row
 
+// Convert takes an io.ReadCloser and returns Tables and an error.
+// The Tables are Rows, where each Row is a hashmap with the header string has the key.
+// If there is an error reading from the input or parsing the HTML, Convert returns the Tables processed so far and the error.
 func Convert(reader io.ReadCloser) ([]Table, error) {
 	tables := []Table{}
 
@@ -40,12 +48,56 @@ func Convert(reader io.ReadCloser) ([]Table, error) {
 						row[header] = strings.TrimSpace(dataSection.Text())
 					}
 				})
-				table = append(table, row)
+				if len(row) > 0 {
+					table = append(table, row)
+				}
 			}
 		})
 
-		tables = append(tables, table)
+		if len(table) > 0 {
+			tables = append(tables, table)
+		}
 	})
 
 	return tables, nil
+}
+
+func ConvertUrl(filePath string) ([]Table, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	tables, err := Convert(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return tables, nil
+}
+
+func ConvertReaderToJSON(reader io.ReadCloser) ([]byte, error) {
+	tables, err := Convert(reader)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file: %w", err)
+	}
+
+	json, err := json.Marshal(tables)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return json, nil
+}
+
+func ConvertURLToJSON(filePath string) ([]byte, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	return ConvertReaderToJSON(file)
 }
